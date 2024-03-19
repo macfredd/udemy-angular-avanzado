e05595e3ed779c08f70e6b4e3c873b889f8904e3
Freddy M Cruz Estrada <freddy.cruze@gmail.com>
2024-03-19 06:30:57 -0600

    02-adminPro: Adding barChart

--- 02-adminPro/src/app/pages/pages-routing.module.ts
+++ 02-adminPro/src/app/pages/pages-routing.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
-import { Grafica1Component } from './grafica1/grafica1.component';
+import { Grafica1Component } from './graficas/grafica1/grafica1.component';
+import { Grafica2Component } from './graficas/grafica2/grafica2.component';
 
 
 const routes: Routes = [
@@ -14,6 +15,7 @@ const routes: Routes = [
           { path: '', component: DashboardComponent },
           { path: 'progress', component: ProgressComponent },
           { path: 'grafica1', component: Grafica1Component },
+          { path: 'grafica2', component: Grafica2Component },
         ],
       },
 ];
--- 02-adminPro/src/app/pages/pages.module.ts
+++ 02-adminPro/src/app/pages/pages.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { Grafica1Component } from './grafica1/grafica1.component';
+import { Grafica1Component } from './graficas/grafica1/grafica1.component';
 import { ProgressComponent } from './progress/progress.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
@@ -9,6 +9,7 @@ import { PagesComponent } from './pages.component';
 import { FormsModule } from '@angular/forms';
 import { ComponentsModule } from '../components/components.module';
 import { BaseChartDirective } from 'ng2-charts';
+import { Grafica2Component } from './graficas/grafica2/grafica2.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { BaseChartDirective } from 'ng2-charts';
     DashboardComponent,
     Grafica1Component,
     ProgressComponent,
-    PagesComponent
+    PagesComponent,
+    Grafica2Component
   ],
   exports: [
     DashboardComponent,
